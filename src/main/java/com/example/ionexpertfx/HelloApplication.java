f06d05de18c187b2b59ff221cc0d51d0680b8116
ElinorTch <tchamoelii@gmail.com>
2025-06-16 10:39:39 +0200

    Feat: Update FXML layout and add button styles with Atlantafx themes

--- src/main/java/com/example/ionexpertfx/HelloApplication.java
+++ src/main/java/com/example/ionexpertfx/HelloApplication.java
@@ -1,8 +1,11 @@
 package com.example.ionexpertfx;
 
+import atlantafx.base.theme.PrimerDark;
+import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +13,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
