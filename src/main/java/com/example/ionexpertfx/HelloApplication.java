a5505391bdabdb842128afe917f14782bf53c2f0
ElinorTch <tchamoelii@gmail.com>
2025-06-16 13:39:55 +0200

    Feat: Update FXML layout and implement custom theme support

--- src/main/java/com/example/ionexpertfx/HelloApplication.java
+++ src/main/java/com/example/ionexpertfx/HelloApplication.java
@@ -1,21 +1,23 @@
 package com.example.ionexpertfx;
 
-import atlantafx.base.theme.PrimerDark;
 import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static PseudoClass CUSTOM_THEME = PseudoClass.getPseudoClass("custom-theme");
+
     @Override
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getRoot().pseudoClassStateChanged(CUSTOM_THEME, true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
